a0d52d06a8b4abe55abf004fcae23bbf9decae5f
hannwe033 <hannwe064@gmail.com>
2022-11-26 04:11:03 +0630

    add new file

---
+++ javaScript/main.js
@@ -0,0 +1,46 @@
+// alert("Hello Best 5");
+// clear();
+// console.log("Hello Best 5");
+// console.log(300);
+// console.error("This is error message")
+// console.warn("This is warning message")
+// let name = "Han Nwe Soe";
+// let age = 27;
+// age = 30;
+// age =15;
+// name = "HTML";
+// console.log(age);
+// console.log(name);
+// const city = "Yangon";
+// city = "Mdy";
+// console.log(city);
+// const name = "Han Nwe soe";
+// const age = 24;
+// const rating = 1.5;
+// const isCool = true;
+// const x = null;
+// let z;
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isCool);
+// console.log(typeof x);
+// console.log(typeof z);
+// const s = "Hello Batch 5";
+
+// console.log(s);
+// console.log(s.length);
+// console.log(s.toUpperCase ());
+// console.log(s.toLowerCase ());
+// console.log(s.substring (0,5) .toUpperCase ());
+// console.log(s.substring (6,13) .toUpperCase ());
+// console.log(s.split(""));
+// concatenation
+const firstName = "HanNwe";
+const lastName = "Soe";
+const age = 24;
+console.log("My name is" +" "+ firstName +lastName+ ". I am" +" "+age + "years old.");
+
+// template string
+console.log(`My name is ${firstName}${lastName}. I am ${age} years old.`)
+
