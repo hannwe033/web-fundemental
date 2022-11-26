7d8e4fb20a988e391f5efa95696d2346b03377a5
hannwe033 <hannwe064@gmail.com>
2022-11-26 22:42:38 +0630

    change

--- javaScript/main.js
+++ javaScript/main.js
@@ -35,12 +35,76 @@
 // console.log(s.substring (0,5) .toUpperCase ());
 // console.log(s.substring (6,13) .toUpperCase ());
 // console.log(s.split(""));
-// concatenation
-const firstName = "HanNwe";
-const lastName = "Soe";
-const age = 24;
-console.log("My name is" +" "+ firstName +lastName+ ". I am" +" "+age + "years old.");
+// // concatenation
+// const firstName = "HanNwe";
+// const lastName = "Soe";
+// const age = 24;
+// console.log("My name is" +" "+ firstName +lastName+ ". I am" +" "+age + "years old.");
 
 // template string
-console.log(`My name is ${firstName}${lastName}. I am ${age} years old.`)
+// console.log(`My name is ${firstName}${lastName}. I am ${age} years old.`)
+
+// const animals = { name: "Dalier", age: 2, type: "Cat" };
+// console.log(animals);
+// console.log(animals.name);
+// console.log(animals.age);
+// console.log(animals.type);
+
+// const person = {
+//     firstName: "Han",
+//     middleName: "Nwe",
+//     lastName: "Soe",
+//     age: 24,
+//     gender: "F",
+//     hobbies: ["music", "movies","sports"],
+//     address: {
+//         number: "no.37",
+//         street: "Bo",
+//         township: "Dala",
+//         city: "Yangon",
+//      },
+//     email: "hannwe064@gmail.com",
+//     phone: 09979609621,
+// };
+// person.maritalStatus = "Singel";
+// person.address.country = "Myanmar";
+// person.hobbies[3] = "Song";
+
+// console.log(person);
+// console.log(person.firstName, person.middleName, person.lastName);
+// console.log(person.hobbies[0]);
+// console.log(person.address.township);
+
+const todos = [
+    {
+        id: 1,
+        text: "Take out trash",
+        isComplete: true,
+    },
+    {
+        id: 2,
+        text: "Meeting with boss",
+        isComplete: true,
+    },
+    {
+        id: 3,
+        text: "Go to clinic",
+        isComplete: true,
+    },
+];
+// console.log(todos);
+// console.log(todos[2].text);
+
+// const todoJSON = JSON.stringify(todos);
+// console.log(todoJSON);
+
+// For loop
+for (let i = 0; i < todos.length; i++){
+    console.log(todos[i].id);
+    console.log(todos[i].text);
+    console.log(todos[i].isComplete);
+}
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`for loop number: ${i}`);
+// }
 
